8a2603ae42250f02952a453add05e2ff32521383
Stuksus <itsoluution@gmail.com>
2017-08-02 22:18:47 +0200

    Update list.php

--- list.php
+++ list.php
@@ -1,3 +1,3 @@
 <?php
 echo "<h1>Список тестов</h1>";
-echo "<form method=\"post\" action=\"login.php\"><input type=\"submit\" value=\"Войти\"></form>";
+echo "<form method=\"post\" action=\"admin.php\"><input type=\"submit\" value=\"Войти\"></form>";
